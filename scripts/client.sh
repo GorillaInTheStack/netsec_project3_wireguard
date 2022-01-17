673a35e17fd0bf3941f1b6f3285ccc928c6f5569
sam66ish <ha223cz@student.lnu.se>
2022-01-17 18:28:17 +0100

    Wireguard manual config now works with NAT

--- scripts/client.sh
+++ scripts/client.sh
@@ -10,3 +10,12 @@ ip6tables-save > /etc/iptables/rules.v6
 ## Install app
 cd /home/vagrant/client_app
 npm install
+
+## Configure client server IP to the cloud instead of on-site
+cat > config.json <<EOL
+{
+  "server_ip": "172.30.30.30",
+  "server_port": "8080",
+  "log_file": "/var/log/client.log"
+}
+EOL
